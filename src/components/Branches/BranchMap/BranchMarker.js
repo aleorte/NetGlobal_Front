5a869a654df9d1f83e8022f75be23cff047a3a2c
CrespoRV <rodrigo-cs@live.com.ar>
2022-07-14 02:51:04 -0300

    [feat]: Add polygon to branch map

---
+++ src/components/Branches/BranchMap/BranchMarker.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { Box, Avatar, Typography } from "../../../styles/material";
+import {
+  DirectionsCarIcon,
+  DirectionsWalkIcon,
+} from "../../../styles/materialIcons";
+import { getDistance,walkTime,carTime } from "../../../utils/mapFunctions";
+import { Popup, Marker } from "react-leaflet";
+import { guardIcon } from "./icons";
+
+const BranchMarker = ({ guard, branch,onClick }) => {
+
+  const guardPosition = [guard.coordinateLatitude, guard.coordinateLength];
+  const branchPosition = [branch.coordinateLatitude, branch.coordinateLength];
+  const distance = getDistance(branchPosition, guardPosition)
+
+  return (
+    <Marker
+      key={guard?.id}
+      position={guardPosition}
+      icon={guardIcon}
+      eventHandlers={{
+        click: onClick ,
+      }}
+    >
+      <Popup>
+        <Box display="flex" gap={1} alignItems="center">
+          <Avatar src={guard?.image}></Avatar>
+          <Box display="flex" flexDirection="column" justifyContent="center">
+            <Box display="flex" gap={1}>
+              <Typography
+                variant="body2"
+                sx={{ m: "5px !important" }}
+                fontWeight="bold"
+              >
+                {guard.lastName + " " + guard.name}
+              </Typography>
+              <Typography
+                variant="body2"
+                sx={{ m: "5px !important", color: "gray" }}
+              >
+                {distance} m{" "}
+              </Typography>
+            </Box>
+            <Box
+              display="flex"
+              gap={1}
+              alignItems="center"
+              justifyContent="space-around"
+            >
+              <Box display="flex" alignItems="center">
+                <DirectionsWalkIcon />
+                <Typography
+                  variant="body2"
+                  sx={{ m: "0 !important", color: "gray" }}
+                >
+                  {walkTime(distance)} min
+                </Typography>
+              </Box>
+              <Box display="flex" alignItems="center">
+                <DirectionsCarIcon />
+                <Typography
+                  variant="body2"
+                  sx={{ pl:0.5,m: "0 !important", color: "gray" }}
+                >
+                  {carTime(distance)} min
+                </Typography>
+              </Box>
+            </Box>
+          </Box>
+        </Box>
+      </Popup>
+    </Marker>
+  );
+};
+
+export default BranchMarker;
--- src/components/Branches/BranchMap/index.js
+++ src/components/Branches/BranchMap/index.js
@@ -1,19 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { Popup, Circle, MapContainer, TileLayer } from "react-leaflet";
+import { Popup, Circle, MapContainer, TileLayer,Marker, Polyline } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import { Marker } from "react-leaflet";
 import { useSelector, useDispatch } from "react-redux";
 import { getAvailableGuards } from "../../../state/branch";
-import { Avatar, Box, Typography } from "../../../styles/material";
-import { getDistance } from "../../../utils/mapFunctions";
-import { branchIcon,guardIcon } from "./icons";
-import { DirectionsCarIcon,DirectionsWalkIcon } from "../../../styles/materialIcons";
+import { branchIcon } from "./icons";
+import BranchMarker from "./BranchMarker";
 
 const BranchMap = ({ selected }) => {
   const [map, setMap] = useState(null);
   const dispatch = useDispatch();
   const { guards } = useSelector((state) => state.branch);
   const [branchPosition,setBranchPosition] = useState([0,0])
+  const [linePosition,setLinePosition] = useState([[0,0],[0,0]])
 
   useEffect(() => {
     selected.id && dispatch(getAvailableGuards(selected.id));
@@ -24,6 +22,11 @@ const BranchMap = ({ selected }) => {
     setBranchPosition([selected.coordinateLatitude, selected.coordinateLength])
     map.flyTo([selected.coordinateLatitude, selected.coordinateLength], 14);
   }, [selected, map]);
+
+  const onClick = (e) =>{
+    const { lat, lng } = e.latlng
+    setLinePosition([branchPosition,[lat,lng]])
+  }
 
   return (
     selected.id &&
@@ -46,38 +49,13 @@ const BranchMap = ({ selected }) => {
           radius={15209}
           center={branchPosition}
         />
+        <Polyline
+          positions={linePosition}
+        />
 
         {guards.map((guard) => {
-
-          const guardPosition = [guard.coordinateLatitude, guard.coordinateLength]
           return (
-            <Marker
-              key={guard?.id}
-              position={[guard.coordinateLatitude, guard.coordinateLength]}
-              icon={guardIcon}
-            >
-                <Popup>
-                    <Box display="flex" gap={1} alignItems="center">
-                        <Avatar src={guard?.image}></Avatar>
-                        <Box display="flex" flexDirection="column" justifyContent="center">
-                            <Box display="flex" gap={1}>
-                                <Typography variant="body2" sx={{m:"5px !important"}} fontWeight="bold">{guard.lastName + " " + guard.name}</Typography>
-                                <Typography variant="body2" sx={{m:"5px !important",color:"gray"}}> { getDistance(branchPosition,guardPosition) } m </Typography>
-                            </Box>
-                            <Box display="flex" gap={1} alignItems="center" justifyContent="space-around">
-                                <Box display="flex" alignItems="center">                   
-                                    <DirectionsWalkIcon/>
-                                    <Typography variant="body2" sx={{m:"0 !important",color:"gray"}}> 160 min </Typography>
-                                </Box>
-                                <Box display="flex" alignItems="center">
-                                    <DirectionsCarIcon/>
-                                    <Typography variant="body2" sx={{m:"0 !important",color:"gray"}}> 20 min </Typography> 
-                                </Box>  
-                            </Box>
-                        </Box>
-                    </Box>
-                </Popup>
-            </Marker>
+            <BranchMarker key={guard?.id} guard={guard} branch={selected} onClick={onClick}/>
           );
         })}
       </MapContainer>
